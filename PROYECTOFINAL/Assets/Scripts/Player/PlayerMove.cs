18d5b403803c2f36dd874c27d2c3994eec5f6115
TatianaSysiuk <111063723+TatianaSysiuk@users.noreply.github.com>
2022-08-22 09:49:37 -0300

    Scenes
    División de escenas

--- PROYECTOFINAL/Assets/Scripts/Player/PlayerMove.cs
+++ PROYECTOFINAL/Assets/Scripts/Player/PlayerMove.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-      [SerializeField]
+    [SerializeField]
     [Range(1f, 10f)]
     private float speed = 3f;
 
+    private bool canJump = true;
+    public bool CanJump { get => canJump; set => canJump = value; }
 
+    [SerializeField] private int JumpHeight = 4;
+
+
+    private bool applyGravity = false;
+    private Vector3 playerDirection;
 
     void Start()
     {
@@ -18,7 +25,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetKey(KeyCode.D))
         {
             MovePlayer(Vector3.forward);
@@ -28,13 +35,33 @@ public class PlayerMove : MonoBehaviour
         {
             MovePlayer(Vector3.back);
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            canJump = false;
+            // playerDirection += Vector3.up * 50;
+        }
+
+        if (applyGravity) playerDirection += Vector3.down;
+        if (transform.position.y <= 0 && canJump) applyGravity = false;
+
+        if (!canJump)
+        {
+            if (transform.position.y <= JumpHeight)
+            {
+                playerDirection += Vector3.up;
+            }
+            else
+            {
+                applyGravity = true;
+            }
+
+        }
+
+        void MovePlayer(Vector3 direction)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
     }
 
-    private void MovePlayer(Vector3 direction)
-    {
-        transform.Translate(direction * speed * Time.deltaTime);
     }
-
-   
-}
