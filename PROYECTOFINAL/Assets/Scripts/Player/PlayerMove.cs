da7362a2e615b449f81535940fae3ef9497ac2bf
TatianaSysiuk <111063723+TatianaSysiuk@users.noreply.github.com>
2022-08-27 21:42:43 -0300

    PlayerMove

--- PROYECTOFINAL/Assets/Scripts/Player/PlayerMove.cs
+++ PROYECTOFINAL/Assets/Scripts/Player/PlayerMove.cs
@@ -11,44 +11,51 @@ public class PlayerMove : MonoBehaviour
     private bool canJump = true;
     public bool CanJump { get => canJump; set => canJump = value; }
 
-    /*[SerializeField] private int JumpHeight = 4;
+    [SerializeField] private int JumpHeight = 4;
 
 
-    private bool applyGravity = false;*/
+    
     private Vector3 playerDirection;
 
+    [SerializeField] Animator playerAnimator;
+
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
 
         if (Input.GetKey(KeyCode.D))
+        {
             MovePlayer(Vector3.forward);
+            if (!IsAnimation("FORWARD")) playerAnimator.SetTrigger("FORWARD");
+            
+
+        }
+
 
         if (Input.GetKey(KeyCode.A))
-            MovePlayer(Vector3.back);
+        {
+            MovePlayer(Vector3.back); 
+            if (!IsAnimation("BACK")) playerAnimator.SetTrigger("BACK");
+            
 
-        /*if (Input.GetKeyDown(KeyCode.Space) && canJump){
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump){
             canJump = false;
             playerDirection += Vector3.up * 50;
         }
 
-        if (applyGravity) playerDirection += Vector3.down;
-        if (transform.position.y <= 0 && canJump) applyGravity = false;
-
-        if (!canJump)
-        {
-            if (transform.position.y <= JumpHeight)
-            {
-                playerDirection += Vector3.up;
-            }
-            else
-            {
-                applyGravity = true;
-            }
-
-        }*/
+        
     }
 
-    void MovePlayer(Vector3 direction){
+    private bool IsAnimation(string animName)
+    {
+        return playerAnimator.GetCurrentAnimatorStateInfo(0).IsName(animName);
+    }
+
+    void MovePlayer(Vector3 direction)
+    {
         transform.Translate(direction * speed * Time.deltaTime);
     }
 }
