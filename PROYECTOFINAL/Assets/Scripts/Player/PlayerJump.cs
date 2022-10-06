d699687c2d1b15231a9b9f857b18b8ef01d0237c
TatianaSysiuk <111063723+TatianaSysiuk@users.noreply.github.com>
2022-10-05 23:24:13 -0300

    Player

--- PROYECTOFINAL/Assets/Scripts/Player/PlayerJump.cs
+++ PROYECTOFINAL/Assets/Scripts/Player/PlayerJump.cs
@@ -14,7 +14,7 @@ public class PlayerJump : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        puedoSaltar = false;
+        
         anim = GetComponent<Animator>();
     }
 
@@ -27,23 +27,34 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (puedoSaltar)
-        {
-            if (Input.GetKeyDown(KeyCode.Space)){
+        
+        
+           if (Input.GetKeyDown(KeyCode.Space)){
 
                 anim.SetBool("SALTE", true);
                 rb.AddForce(new Vector3(0,FuerzaDeSalto,0),ForceMode.Impulse);
+                
+
+            }
+            anim.SetBool("TOCARSUELO",true);
+    
+    
+    }
+     public void OnCollisionStay(Collision other) {
+        if (other.gameObject.CompareTag("piso"))
+        {
+             if (Input.GetKeyDown(KeyCode.Space)){
+
+                anim.SetBool("SALTE", true);
+                rb.AddForce(new Vector3(0,FuerzaDeSalto,0),ForceMode.Impulse);
+                
 
             }
             anim.SetBool("TOCARSUELO",true);
         }
-        else{
-            EstoyCayendo();
-        }
+        
     }
 
-    public void EstoyCayendo(){
-        anim.SetBool("TOCARSUELO", false);
-        anim.SetBool("SALTE", false);
-    }
+    
+    
 }
